99d4e7064efdc75d446b3d70544e28fb54628579
th19xcf <th19xcf@gmail.com>
2026-04-26 11:29:20 +0800

    2026-4-26，修改，首页内容

--- frontend/src/locales/langs/zh-cn.ts
+++ frontend/src/locales/langs/zh-cn.ts
@@ -284,7 +284,7 @@ const local: App.I18n.Schema = {
     },
     home: {
       branchDesc:
-        '为了方便大家开发和更新合并，我们对main分支的代码进行了精简，只保留了首页菜单，其余内容已移至example分支进行维护。预览地址显示的内容即为example分支的内容。',
+        '新版本已发布，欢迎使用！',
       greeting: '早安，{userName}, 今天又是充满活力的一天!',
       weatherDesc: '今日多云转晴，20℃ - 25℃!',
       projectCount: '项目数',
@@ -303,11 +303,11 @@ const local: App.I18n.Schema = {
       projectNews: {
         title: '项目动态',
         moreNews: '更多动态',
-        desc1: 'Soybean 在2021年5月28日创建了开源项目 soybean-admin!',
-        desc2: 'Yanbowe 向 soybean-admin 提交了一个bug，多标签栏不会自适应。',
-        desc3: 'Soybean 准备为 soybean-admin 的发布做充分的准备工作!',
-        desc4: 'Soybean 正在忙于为soybean-admin写项目说明文档！',
-        desc5: 'Soybean 刚才把工作台页面随便写了一些，凑合能看了！'
+        desc1: '测试',
+        desc2: '',
+        desc3: '',
+        desc4: '',
+        desc5: ''
       },
       creativity: '创意'
     },
