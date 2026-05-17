2e6a68375cf9745168d9217e6c5874c022ec606a
th19xcf <th19xcf@gmail.com>
2026-05-17 12:36:50 +0800

    2026-5-17，修改，优化数据库连接，启用持久连接和单例模式

--- backend/app/Models/Mcommon.php
+++ backend/app/Models/Mcommon.php
@@ -1,158 +1,135 @@
 <?php
 
-/* v2.4.1.1.202510081330, from home */
-
 namespace App\Models;
+
 use CodeIgniter\Model;
 
 class Mcommon extends Model
 {
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    // 通用查询
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    public function select($sql)
-    {
-        $db = db_connect('btdc');
-        $query = $db->query($sql);
-        $db->close();
+    protected $DBGroup = 'btdc';
 
-        return $query;
+    private ?object $dbInstance = null;
+
+    private function getDb(): object
+    {
+        if ($this->dbInstance === null) {
+            $this->dbInstance = db_connect('btdc');
+        }
+        return $this->dbInstance;
     }
 
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    // 通用更新
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    public function modify($sql)
+    public function select(string $sql)
     {
-        $db = db_connect('btdc');
+        $db = $this->getDb();
+        return $db->query($sql);
+    }
 
+    public function modify(string $sql): int
+    {
+        $db = $this->getDb();
         $db->query($sql);
-        $num = $db->affectedRows();
-
-        $db->close();
-
-        return $num;
+        return $db->affectedRows();
     }
 
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    // 通用插入
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    public function add($table, $data, $fld_arr)
+    public function add(string $table, array $data, array $fld_arr): int
     {
-        $db = db_connect('btdc');
+        $db = $this->getDb();
+        $num = 0;
 
-        foreach ($data as $arr)
-        {
-            if (!array_diff($arr, $fld_arr)) continue;  //表头
-            $arr = array_combine($fld_arr, $arr);  //修改键名
+        foreach ($data as $arr) {
+            if (!array_diff($arr, $fld_arr)) {
+                continue;
+            }
+            $arr = array_combine($fld_arr, $arr);
             $db->table($table)->insert($arr);
             $num = $db->affectedRows();
         }
 
-        $db->close();
-
         return $num;
     }
 
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    // 使用事务方式插入
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    public function add_by_trans($table, $data, $col_arr, $fld_arr)
+    public function add_by_trans(string $table, array $data, array $col_arr, array $fld_arr): int
     {
-        $db = db_connect('btdc');
+        $db = $this->getDb();
 
         $db->transStart();
 
         $num = 0;
-        foreach ($data as $arr)
-        {
+        foreach ($data as $arr) {
             $db->table($table)->insert($arr);
-            $num = $num + $db->affectedRows();
+            $num += $db->affectedRows();
         }
 
         $db->transComplete();
 
-        if ($this->db->transStatus() == false)
-        {
+        if ($db->transStatus() === false) {
             log_message('error', '事务执行错误');
             return -1;
         }
 
-        $db->close();
-
         return $num;
     }
 
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    // 通用执行sql
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    public function exec($sql)
+    public function exec(string $sql): int
     {
-        $db = db_connect('btdc');
-
+        $db = $this->getDb();
         $db->query($sql);
-        $num = $db->affectedRows();
-
-        $db->close();
-
-        return $num;
+        return $db->affectedRows();
     }
 
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    // 导入前处理模块,存储过程调用
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    public function import_before_sp($sp, &$param = null)
+    public function import_before_sp(string $sp, ?string &$param = null): object
     {
-        $db = db_connect('btdc');
+        $db = $this->getDb();
         $query = $db->query($sp);
         $out = $db->query('select @out')->getResultArray();
-        if (count($out)>0)
-        {
+        if (count($out) > 0) {
             $param = current($out[0]);
         }
-
-        $db->close();
 
         return $query;
     }
 
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    // 读出字段
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    public function get_fields($table_name)
+    public function get_fields(string $table_name): array
     {
-        $db = db_connect('btdc');
-        $fields = $db->getFieldNames($table_name);
-        $db->close();
-
-        return $fields;
+        $db = $this->getDb();
+        return $db->getFieldNames($table_name);
     }
 
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    // 通用日志sql
-    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    public function sql_log($option, $func_id='', $info='')
+    public function sql_log(string $option, string $func_id = '', string $info = ''): int
     {
         $session = \Config\Services::session();
         $user_name = $session->get('user_name');
         $user_workid = $session->get('user_workid');
         $log_switch = $session->get('log_switch');
 
-        if (!$log_switch) return 0; // 不写日志
+        if (!$log_switch) {
+            return 0;
+        }
 
-        $db = db_connect('btdc');
+        $db = $this->getDb();
 
-        $insert = sprintf('
-            insert into sys_sql_log
-            (姓名,用户名,动作,功能编码,信息)
-            values ("%s","%s","%s","%s","%s")',
-            $user_name, $user_workid, $option, $func_id, $info);
+        $insert = sprintf(
+            'insert into sys_sql_log (姓名,用户名,动作,功能编码,信息) values ("%s","%s","%s","%s","%s")',
+            $user_name,
+            $user_workid,
+            $option,
+            $func_id,
+            $info
+        );
 
         $db->query($insert);
-        $num = $db->affectedRows();
+        return $db->affectedRows();
+    }
 
-        $db->close();
+    public function quote(string $value): string
+    {
+        $db = $this->getDb();
+        return $db->escape($value);
+    }
 
-        return $num;
+    public function getLastQuery(): ?string
+    {
+        $db = $this->getDb();
+        return (string) $db->getLastQuery();
     }
 }
