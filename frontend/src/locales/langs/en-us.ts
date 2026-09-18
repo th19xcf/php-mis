a7a23e17fb06e8819f4dbf96525fb75add1c87b2
th19xcf <th19xcf@gmail.com>
2026-09-18 14:57:25 +0800

    2026-9-18，修改，style: 合同管理菜单去掉 V2 后缀，更名为「合同管理」

--- frontend/src/locales/langs/en-us.ts
+++ frontend/src/locales/langs/en-us.ts
@@ -254,7 +254,7 @@ const local: App.I18n.Schema = {
     personnel_train: 'Training Personnel',
     personnel_employee: 'Employee Management',
     personnel_person: 'Person Archive Management',
-    'contract-v2': 'Contract Management V2',
+    'contract-v2': 'Contract Management',
     'workflow-manage': 'Workflow Management',
     'oa-todo-center': 'Todo Center',
     'room-status': 'Room Status'
